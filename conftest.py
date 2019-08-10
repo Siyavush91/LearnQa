06c12473fc3415c40a7ba0b0eeb41b1b961e83ec
Seva <vsiyavush91@gmail.com>
2019-08-10 19:48:40 +0300

    Add changes in main conftest file

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
-# import pytest
-#
-# @pytest.fixture(autouse=True)
-# def always_used_fixture(request):
-#     print(f"\n Always, HI!")
-#
-#     def second():
-#         print(f"\n Do something, after test")
-#
-#     request.addfinalizer(second)
+import pytest
+
+@pytest.fixture(autouse=True)
+def always_used_fixture(request):
+    print(f"\n Always, HI!")
+
+    def second():
+        print(f"\n Do something, after test")
+
+    request.addfinalizer(second)
